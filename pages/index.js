1911c414bedb08ff9b3ae8c5eaf6db3d524d2d6d
sorious77 <sorious77@naver.com>
2022-01-14 23:47:16 +0900

    #2.2 Redirects and Rewrites

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
 import { useEffect, useState } from "react";
 import Header from "../Components/Header";
 
-const API_KEY = "";
-
 export default () => {
   const [movies, setMovies] = useState();
 
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch("/api/movies")).json();
 
       setMovies(results);
     })();
@@ -20,13 +14,38 @@ export default () => {
 
   return (
     <div>
-      <Header title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
-        <div key={movie.id}>
-          <h4>{movie.original_title}</h4>
-        </div>
-      ))}
+      <div className="container">
+        <Header title="Home" />
+        {!movies && <h4>Loading...</h4>}
+        {movies?.map((movie) => (
+          <div key={movie.id} className="movie">
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+            <h4>{movie.original_title}</h4>
+          </div>
+        ))}
+      </div>
+
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 };
