66649c19a764c84a6d4f99c05bf4fd156a81446b
sorious77 <sorious77@naver.com>
2022-01-14 23:17:17 +0900

    #2.1 Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+import Header from "../Components/Header";
+
+const API_KEY = "";
+
 export default () => {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
       <Header title="Home" />
-      <h1>Home</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 };
